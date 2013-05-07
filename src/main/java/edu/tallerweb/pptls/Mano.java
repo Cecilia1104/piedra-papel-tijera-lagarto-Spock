caa68b0b471796234153496cad26d38f318b3aba
Cecilia1104 <ce.gisele84@gmail.com>
2013-05-07 15:43:52 -0300

    Resolucion del Trabajo Practico (cambios)

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,32 +4,43 @@ public class Mano {
 
 	private Forma forma;
 
-
 	public Mano(final Forma forma) {
-		this.forma=forma;
+		this.forma = forma;
 	}
 
 	public Resultado jugarCon(final Mano otra) {
-	
-		if(this.forma==otra.forma)
-		  return Resultado.EMPATA;
-		else
-		  if(this.forma==Forma.SPOCK && (otra.forma==Forma.TIJERA || otra.forma==Forma.PIEDRA))
-	        return Resultado.GANA;
-		  else
-			 if(this.forma==Forma.PIEDRA && (otra.forma==Forma.TIJERA || otra.forma==Forma.LAGARTO))
+	 if (this.forma == otra.forma)
+	 {
+      return Resultado.EMPATA;
+	 }
+	 else
+	   if (this.forma == Forma.SPOCK && (otra.forma == Forma.TIJERA || otra.forma == Forma.PIEDRA))
+	    {
+		   return Resultado.GANA;
+	    }
+	   else
+		 if (this.forma == Forma.PIEDRA && (otra.forma == Forma.TIJERA || otra.forma == Forma.LAGARTO))
+		  {
+			 return Resultado.GANA;
+		  }
+		 else
+		   if (this.forma == Forma.LAGARTO && (otra.forma == Forma.SPOCK || otra.forma == Forma.PAPEL))
+		    {
 			   return Resultado.GANA;
+		    }
+		   else
+		     if (this.forma == Forma.PAPEL && (otra.forma == Forma.PIEDRA || otra.forma == Forma.SPOCK))
+			  {
+		    	 return Resultado.GANA;
+			  }
 			 else
-			    if(this.forma==Forma.LAGARTO && (otra.forma==Forma.SPOCK || otra.forma==Forma.PAPEL))
-				 return Resultado.GANA;
-			    else
-				  if(this.forma==Forma.PAPEL && (otra.forma==Forma.PIEDRA ||otra.forma==Forma.SPOCK))
+			   if (this.forma == Forma.TIJERA && (otra.forma == Forma.PAPEL || otra.forma == Forma.LAGARTO))
+				{
 				   return Resultado.GANA;
-				  else
-					if(this.forma==Forma.TIJERA && (otra.forma==Forma.PAPEL || otra.forma==Forma.LAGARTO))
-					  return Resultado.GANA;
-					else
-					   return Resultado.PIERDE;
-		       
+				}
+			   else
+			   {
+				 return Resultado.PIERDE;
+			   }
       }
 }
